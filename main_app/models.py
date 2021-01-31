a9870888fd899af1fb724c488df8b14ba001a76f
mcholbrook <mirandacholbrook@gmail.com>
2021-01-31 12:10:15 -0500

    Add item model and routing for gear index

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+CAMPING = (
+  ('C', 'Car Camping'),
+  ('B', 'Backpacking')
+)
+
 # Create your models here.
+class Item(models.Model):
+  name = models.CharField(max_length=100)
+  description = models.TextField(max_length=250)
+  category = models.CharField(max_length=100)
+  notes = models.TextField(max_length=500)
+  typeofcamping = models.CharField(
+    max_length=1,
+    choices=CAMPING,
+    default=CAMPING[0][0]
+  )
+
+  def __str__(self):
+    return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -3,7 +3,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-  return HttpResponse('<h1>This is the gear collector homepage!</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
+
+def gear_index(request):
+  return render(request, 'gear/index.html')
